1570e549329c22a207abc0595432aad97f0b9a47
Lucas Lucovas <lucaslucovas@gmail.com>
2022-07-21 01:17:37 -0300

    subiendo logica de paginacion

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const API = `https://api.escuelajs.co/api/v1/products?offset=0&limit=10`
+
+const API = `https://api.escuelajs.co/api/v1/products?`
+
+let previousPageButton = document.querySelector('.previousPage')
+let nextPageButton = document.querySelector('.nextPage')
+let div = document.querySelector('.products')
+let pageCounter = 10
 
 function fetchData(urlApi){
     return fetch(urlApi)
@@ -6,11 +12,11 @@ function fetchData(urlApi){
 
 
 
-fetchData(API)
+
+fetchData(`${API}offset=10&limit=10`)
 .then(response => response.json())
 .then(products => {
-    let div = document.querySelector('.product')
-    console.log(products)
+    div.innerHTML = ''
     products.map((product)=>{
         div.innerHTML += `
         <div class="product">
@@ -19,7 +25,60 @@ fetchData(API)
             <p>Price: $${product.price}</p>
             <p>Description: ${product.description}</p>
             <img src=" ${product.images}" alt="">
+            <button class="cartButton">Add to cart</button>
         </div>
         `
     })
 })
+.catch(error => console.error(error))
+
+
+previousPageButton.addEventListener('click',()=>{
+    pageCounter -= 10
+    if(pageCounter > 10){
+        fetchData(`${API}offset=${pageCounter}&limit=10`)
+        .then(response => response.json())
+        .then(products => {
+            div.innerHTML = ''
+            products.map((product)=>{
+                div.innerHTML += `
+                <div class="product">
+                    <h2>Title: ${product.title}</h2>
+                    <p>Category: ${product.category.name}</p>
+                    <p>Price: $${product.price}</p>
+                    <p>Description: ${product.description}</p>
+                    <img src=" ${product.images}" alt="">
+                    <button class="cartButton">Add to cart</button>
+                </div>
+                `
+            })
+        })
+        .catch(error => console.error(error))
+    }else if(pageCounter < 10){
+        div.innerHTML = `<h2>You can't go back any more. There is nothing to show here</h2>`
+    }
+})
+
+nextPageButton.addEventListener('click',()=>{
+    pageCounter += 10
+    fetchData(`${API}offset=${pageCounter}&limit=10`)
+    .then(response => response.json())
+    .then(products => {
+        div.innerHTML = ''
+        products.map((product)=>{
+            div.innerHTML += `
+            <div class="product">
+                <h2>Title: ${product.title}</h2>
+                <p>Category: ${product.category.name}</p>
+                <p>Price: $${product.price}</p>
+                <p>Description: ${product.description}</p>
+                <img src=" ${product.images}" alt="">
+                <button class="cartButton">Add to cart</button>
+            </div>
+            `
+        })
+    })
+    .catch(error => console.error(error))
+})
+
+
