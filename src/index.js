f5302b49fa50e6e9a671447197e739e85d8eba33
Lucas Lucovas <lucaslucovas@gmail.com>
2022-07-26 19:32:50 -0300

    subiendo logica de carrito falta terminar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
-const API = `https://api.escuelajs.co/api/v1/products?`
+const API = `https://api.escuelajs.co/api/v1/products`
 
 let previousPageButton = document.querySelector('.previousPage')
 let nextPageButton = document.querySelector('.nextPage')
 let div = document.querySelector('.products')
+
 let pageCounter = 10
+let cart = []
 
 function fetchData(urlApi){
     return fetch(urlApi)
@@ -13,22 +15,36 @@ function fetchData(urlApi){
 
 
 
-fetchData(`${API}offset=10&limit=10`)
+fetchData(`${API}?offset=10&limit=10`)
 .then(response => response.json())
 .then(products => {
+
     div.innerHTML = ''
     products.map((product)=>{
-        div.innerHTML += `
-        <div class="product">
-            <h2>Title: ${product.title}</h2>
-            <p>Category: ${product.category.name}</p>
-            <p>Price: $${product.price}</p>
-            <p>Description: ${product.description}</p>
-            <img src=" ${product.images}" alt="">
-            <button class="cartButton">Add to cart</button>
-        </div>
+        const divProduct = document.createElement('div')
+        divProduct.classList.add('product')
+        divProduct.innerHTML = `
+        <h2>Title: ${product.title}</h2>
+        <p>Category: ${product.category.name}</p>
+        <p>Price: $${product.price}</p>
+        <p>Description: ${product.description}</p>
+        <img src=" ${product.images}" alt="">
+        <button id="add${product.id}" class="cartButton">+ Add to cart</button>
         `
+        div.appendChild(divProduct)
+
+        let cartButton = document.getElementById(`add${product.id}`)
+        cartButton.addEventListener('click',() => {
+            addToCart(product.id)
+            console.log(cart)
+        })
     })
+
+    const addToCart = (productId)=>{
+        const item = products.find((product) => product.id === productId)
+        cart.push(item)  
+    }
+
 })
 .catch(error => console.error(error))
 
@@ -36,7 +52,7 @@ fetchData(`${API}offset=10&limit=10`)
 previousPageButton.addEventListener('click',()=>{
     pageCounter -= 10
     if(pageCounter > 10){
-        fetchData(`${API}offset=${pageCounter}&limit=10`)
+        fetchData(`${API}?offset=${pageCounter}&limit=10`)
         .then(response => response.json())
         .then(products => {
             div.innerHTML = ''
@@ -48,7 +64,7 @@ previousPageButton.addEventListener('click',()=>{
                     <p>Price: $${product.price}</p>
                     <p>Description: ${product.description}</p>
                     <img src=" ${product.images}" alt="">
-                    <button class="cartButton">Add to cart</button>
+                    <button id="add${product.id}"  class="cartButton">+ Add to cart</button>
                 </div>
                 `
             })
@@ -61,7 +77,7 @@ previousPageButton.addEventListener('click',()=>{
 
 nextPageButton.addEventListener('click',()=>{
     pageCounter += 10
-    fetchData(`${API}offset=${pageCounter}&limit=10`)
+    fetchData(`${API}?offset=${pageCounter}&limit=10`)
     .then(response => response.json())
     .then(products => {
         div.innerHTML = ''
@@ -73,12 +89,14 @@ nextPageButton.addEventListener('click',()=>{
                 <p>Price: $${product.price}</p>
                 <p>Description: ${product.description}</p>
                 <img src=" ${product.images}" alt="">
-                <button class="cartButton">Add to cart</button>
+                <button id="add${product.id}"  class="cartButton">+ Add to cart</button>
             </div>
             `
         })
     })
     .catch(error => console.error(error))
 })
+
+
 
 
