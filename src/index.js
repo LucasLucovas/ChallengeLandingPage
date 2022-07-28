21f3c8b3a3a1f9cd6975123d09134780c93b5879
Lucas Lucovas <lucaslucovas@gmail.com>
2022-07-28 20:05:01 -0300

    subiendo cambios al carrito de compras

--- src/index.js
+++ src/index.js
@@ -4,14 +4,62 @@ const API = `https://api.escuelajs.co/api/v1/products`
 let previousPageButton = document.querySelector('.previousPage')
 let nextPageButton = document.querySelector('.nextPage')
 let div = document.querySelector('.products')
-const cartContainer = document.getElementById('cartContainer')
+let cartContainer = document.getElementById('cartContainer')
+let emptyButton = document.getElementById('emptyCart')
+let cartCounter = document.getElementById('cartCounter')
+let totalAmount = document.getElementById('totalAmount')
 
 let pageCounter = 10
 let cart = []
 
+document.addEventListener('DOMContentLoaded',()=>{
+    if(localStorage.getItem('cart')){
+        cart = JSON.parse(localStorage.getItem('cart'))
+        updateCart()
+    }
+})
+
+
+
+
 function fetchData(urlApi){
     return fetch(urlApi)
 }
+
+emptyButton.addEventListener('click',()=>{
+    cart.length = 0
+    updateCart()
+})
+
+const updateCart = ()=>{
+    cartContainer.innerHTML = ""
+
+    cart.map((product) =>{
+        const div = document.createElement('div')
+        div.className = ('cartProduct')
+        div.innerHTML = `
+            <h2>Name: ${product.title}</h2>
+            <p>Category: ${product.category.name}</p>
+            <p>Price: $${product.price}</p>
+            <p>Amount: <input type="number" min="1" max="30" value="1" id="Amount"></p>
+            <button onclick="deleteCart(${product.id})" class="deleteButton"></button>
+        `
+        cartContainer.appendChild(div)
+        localStorage.setItem('cart',JSON.stringify(cart))
+    
+    })
+
+    cartCounter.innerText = cart.length
+    totalAmount.innerText =  cart.reduce((acc, product) => acc + product.price, 0)
+}
+
+const deleteCart = (productId) =>{
+    const item = cart.find((product)=> product.id === productId )
+    const index = cart.indexOf(item)
+    cart.splice(index,1)
+    updateCart()
+}
+
 
 
 
@@ -40,11 +88,20 @@ fetchData(`${API}?offset=10&limit=10`)
             addToCart(product.id)
             console.log(cart)
         })
+
     })
 
     const addToCart = (productId)=>{
-        const item = products.find((product) => product.id === productId)
-        cart.push(item)  
+        const exist = cart.some(product => product.id === productId)
+
+        if(exist){
+            
+            
+        }else{
+            const item = products.find((product) => product.id === productId)
+            cart.push(item)
+        }
+        updateCart() 
     }
 
 })
@@ -90,13 +147,14 @@ previousPageButton.addEventListener('click',()=>{
             
             const addToCart = (productId)=>{
                 const item = products.find((product) => product.id === productId)
-                cart.push(item)  
+                cart.push(item) 
             }
+
         })
         .catch(error => console.error(error))
 
     }else if(pageCounter < 10){
-        div.innerHTML = `<h2>You can't go back any more. There is nothing to show here</h2>`
+        div.innerHTML = `<h2>You can't go back any more. There is nothing to see here</h2>`
     }
 })
 
@@ -144,18 +202,3 @@ nextPageButton.addEventListener('click',()=>{
 
 
 
-
-const updateCart = ()=>{
-
-    cart.map((product) =>{
-        const div = document.createElement('div')
-        div.className.add('cartProduct')
-        div.innerHTML = `
-            <h2>Title: ${product.title}</h2>
-            <p>Category: ${product.category.name}</p>
-            <p>Price: $${product.price}</p>
-            <p>Amount: <span id="Amount">${product.amount}</span></p>
-            <button onclick="eliminarDelCarrito(${product.id})" class="deleteButton"></button>
-        `
-    })
-}
